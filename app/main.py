c4eabca6ca58d65298a443eecf1bfce277855ec1
MaestroVLG <las-vegas1945@yandex.ru>
2024-11-23 02:37:42 +0300

    fast api

--- app/main.py
+++ app/main.py
@@ -1,13 +1,12 @@
 from fastapi import FastAPI
-from app.routers.task import router as task_router
-from app.routers.user import router as user_router
+from app.db import engine, Base
+from app.models import User, Task
 
 app = FastAPI()
+
+# Создание всех таблиц
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 async def welcome():
     return {"message": "Welcome to Taskmanager"}
-
-# Подключение маршрутов
-app.include_router(task_router)
-app.include_router(user_router)
